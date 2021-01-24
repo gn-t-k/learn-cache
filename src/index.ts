15956e4ba43f3684928ed2f5e8a7f222c81fcc90
gn-t-k <gntk1995422@gmail.com>
2021-01-24 22:08:43 +0900

    キャッシュ使い分け実装

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
-import router from "./router";
+import cacheControl from "express-cache-controller";
 
-const app = express();
+const image = express();
+const imageServer = express.static("images");
+image.use(cacheControl({ noStore: true }));
+image.use(imageServer);
+image.listen(8081);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(router);
-
-const port = process.env.PORT || 8080;
-
-app.listen(port);
+const cachedImage = express();
+const cachedImageServer = express.static("cached-images");
+cachedImage.use(cacheControl({ maxAge: 60 * 60 * 24, private: true }));
+cachedImage.use(cachedImageServer);
+cachedImage.listen(8082);
